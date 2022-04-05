e28d82f2649a0c041e06e3c08244e2220f3f9b28
imad idali ouali <96796912+ImadIdaliouali@users.noreply.github.com>
2022-04-05 16:41:51 +0000

    Update struct.h

--- struct.h
+++ struct.h
@@ -1,14 +1,14 @@
 typedef struct Process{
 	int id;
-	int r_time;// temp d'exection
+	int r_time;// temp d'execusion
 	int a_time;// temp d'arrive
-	int att_time;// temp d'attente
-	int priority;
+	int wait_time;// temp d'attente
+	int priority;// Priorite
 	struct Process* next;
 }process;
 typedef struct{
-	process* head;
-	process* tail;
+	process* head;// tete de la file
+	process* tail;// queue de la file
 	int size;
 }File;
 typedef File* file;
