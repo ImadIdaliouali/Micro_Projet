57d301f4037c86be7df6ebbd63400b3af5bb8672
imad idali ouali <96796912+ImadIdaliouali@users.noreply.github.com>
2022-04-09 13:38:10 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "struct.h"
 #include "functions.h"
+/**
+ * Author : Oussama Makouar, Imad Idali Ouali
+ * Date : 05/04/2022
+ * Language : C
+ */
 int main(){
 	file f = init_file();
 	int n;
-	printf("Enter the number of process : ");
+	printf("Enter the number of processes : ");
 	scanf("%d",&n);
 	create_file(f,n);
 	display(f);
-	diagramme_Gant(f,n);
+	Gantt_chart(f);
 	return 0;
 }
