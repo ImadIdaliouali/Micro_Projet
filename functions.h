d830d06e10dd062e863806365bdaeb1cfa923145
imad idali ouali <96796912+ImadIdaliouali@users.noreply.github.com>
2022-04-09 12:31:12 +0000

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,9 +1,10 @@
 process* create_process(process p){
-	process *p1 = (process *)malloc(sizeof(process));
+	process *p1 = (process*)malloc(sizeof(process));
 	p1->id = p.id;
 	p1->r_time = p.r_time;
 	p1->a_time = p.a_time;
 	p1->priority = p.priority;
+	p1->wait_time = 0;
 	p1->next = NULL;
 	return p1;
 }
@@ -11,6 +12,7 @@ file init_file(){
 	file f = (file)malloc(sizeof(File));
 	f->head = NULL;
 	f->tail = NULL;
+	f->size = 0;
 	return f;
 }
 process* pop_process(file f){
@@ -35,22 +37,13 @@ void push_FIFO(file f, process *p){
 	if(f->tail == NULL)
 		f->head = f->tail = p;
 	else{
-		if(f->tail->a_time + f->tail->r_time > p->a_time){ // hadi hiya li ktbdl temp d'arrive
-			p->wait_time = f->tail->a_time + f->tail->r_time - p->a_time;
-			p->a_time = f->tail->a_time + f->tail->r_time;
+		int b = f->tail->a_time + f->tail->r_time;
+		if(b > p->a_time){
+			p->wait_time = b - p->a_time;
+			p->a_time += p->wait_time;
 		}
-		if(f->tail->a_time < p->a_time){
-			f->tail->next = p;
-			f->tail = p;
-		}
-		else{
-			process *before_end = f->head;
-			while(before_end->next->next != NULL){
-				before_end = before_end->next;
-			}
-			before_end->next = p;
-			p->next = f->tail;
-		}
+		f->tail->next = p;
+		f->tail = p;
 	}
 	f->size++;
 }
@@ -58,7 +51,42 @@ void push_SJF(file f, process *p){
 	if(f->tail == NULL)
 		f->head = f->tail = p;
 	else{
-
+		int b;
+		process *tmp = f->head;
+		process *precedent = NULL;
+		bool q = true;
+		while(tmp != NULL && q == true){
+			if(p->a_time < tmp->a_time && tmp->r_time > p->r_time){
+				precedent->next = p;
+				p->next = tmp;
+				b = precedent->a_time + precedent->r_time;
+				p->wait_time = b - p->a_time;
+				p->a_time += p->wait_time;
+				process *count = tmp;
+				process *pre = p;
+				while(count != NULL){
+					count->wait_time += p->r_time;
+					count->a_time += p->r_time;
+					pre = tmp;
+					count = count->next;
+				}
+				q = false;
+			}
+			else{
+				precedent = tmp;
+				if(tmp->next == NULL){
+					b = tmp->a_time + tmp->r_time;
+					if(b > p->a_time){
+						p->wait_time = b - p->a_time;
+						p->a_time += p->wait_time;
+					}
+					tmp->next = p;
+					f->tail = p;
+					q = false;
+				}
+				tmp = tmp->next;
+			}
+		}
 	}
 	f->size++;
 }
@@ -66,7 +94,7 @@ void push_SRTF(file f,process* p){
 	if(f->tail == NULL)
 		f->head = f->tail = p;
 	else{
-
+		
 	}
 	f->size++;
 }
@@ -136,45 +164,104 @@ void create_file(file f, int n){
 void display(file f){
 	process* tmp = f->head;
 	printf("Tableau:\n");
-	printf("+-----------+----------------+-------------------+----------+\n");
-	printf("| Processus | Date d'arrivee | Duree d'execusion | Priorite |\n");
-	for(int i = 1; i <= f->size; i++){
-		printf("+-----------+----------------+-------------------+----------+\n");
-		printf("|    P%d     |        %d       |         %d         |     %d    |\n",i,tmp->a_time - tmp->wait_time,tmp->r_time,tmp->priority);
+	printf("+-----------+----------------+-------------------+----------+----------------+\n");
+	printf("| Processus | Date d'arrivee | Duree d'execusion | Priorite | temp d'attente |\n");
+	while(tmp != NULL){
+		printf("+-----------+----------------+-------------------+----------+----------------+\n");
+		printf("|    P%d     |        %d       |         %d         |     %d    |        %d       |\n", tmp->id, tmp->a_time - tmp->wait_time, tmp->r_time, tmp->priority,tmp->wait_time);
 		tmp = tmp->next;
 	}
-	printf("+-----------+----------------+-------------------+----------+\n");
+	printf("+-----------+----------------+-------------------+----------+----------------+\n");
+}
+// void diagramme_Gant(file f, int n){// push_FIFO
+// 	int i = 0, j, wait_time = 0;
+// 	printf("Diagramme de Gant:\n");
+// 	printf("process  :");
+// 	for(j = 1; j <= f->size; j++){//kadir 1 2 3 4 ...
+// 		printf(" %d", j);
+// 	}
+// 	printf("\n");
+// 	process *p = NULL;
+// 	process *tmp = NULL;
+// 	while(f->tail != NULL){
+// 		while(i < f->head->a_time + f->head->r_time + wait_time){
+// 			printf("%d       ", i);
+// 			if(i <= 9)
+// 				printf(" ");
+// 			printf(":");
+// 			for(j = 0; j < n - (f->size); j++){
+// 				printf("  ");
+// 			}
+// 			printf(" o");
+// 			for(tmp = f->head->next; tmp != NULL && tmp->a_time <= i; tmp = tmp->next){
+// 				printf(" x");
+// 			}
+// 			printf("\n");
+// 			i++;
+// 		}
+// 		if(f->head->next != NULL)
+// 			wait_time = i - f->head->next->a_time;
+// 		p = pop_process(f);
+// 	}
+// }
+process* search_id(file f, int i){
+	process *tmp =f->head;
+	while(tmp != NULL && tmp->id != i)
+		tmp = tmp->next;
+	return tmp;
 }
 void diagramme_Gant(file f, int n){
-	int i = 0;
-	int j;
+	int i = 0, j, k = 1;
 	printf("Diagramme de Gant:\n");
 	printf("process  :");
-	for(j = 1; j <= f->size; j++){//kadir 1 2 3 4 ...
+	for(j = 1; j <= f->size; j++){
 		printf(" %d", j);
 	}
 	printf("\n");
 	process *p = NULL;
 	process *tmp = NULL;
+	process *count = NULL;
 	while(f->tail != NULL){
 		while(i < f->head->a_time + f->head->r_time){
 			printf("%d       ", i);
 			if(i <= 9)
 				printf(" ");
 			printf(":");
-			for(j = 0; j < n - (f->size); j++){
+			for(j = 1; j < k; j++){
 				printf("  ");
 			}
-			printf(" o");
-			//if(f->head->next != NULL){
-				for(tmp = f->head->next; tmp != NULL; tmp = tmp->next){
+			count = f->head;
+			if(f->head->id == k){
+				printf(" o");
+				for(tmp = count->next; tmp != NULL; tmp = tmp->next){
 					if(tmp->a_time - tmp->wait_time <= i)
 						printf(" x");
 				}
-			//}
+			}
+			else{
+				int d = k;
+				process *count = search_id(f, d);
+				while(count != NULL){
+					if(count->id != f->head->id){
+						if(count->a_time - count->wait_time <= i){
+							printf(" x");
+						}
+						else{
+							printf(" o");
+						}
+					}
+					else{
+						printf(" o");
+					}
+					d++;
+					count = search_id(f, d);
+				}
+			}
 			printf("\n");
 			i++;
 		}
+		if(f->head->id == k)
+			k++;
 		p = pop_process(f);
 	}
 }
